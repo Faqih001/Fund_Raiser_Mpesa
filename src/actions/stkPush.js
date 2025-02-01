d026e8fa93d574a41d5c76667e43494dc79802bb
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-01 23:34:00 +0300

    Add comment to clarify token request to Safaricom API in sendStkPush function

--- src/actions/stkPush.js
+++ src/actions/stkPush.js
@@ -42,6 +42,7 @@ export async function sendStkPush(formData) {
       process.env.MPESA_CONSUMER_KEY + ":" + process.env.MPESA_CONSUMER_SECRET
     ).toString("base64");
 
+    // Token request to Safaricom API 
     const tokenResponse = await axios.get(
       "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
       {
