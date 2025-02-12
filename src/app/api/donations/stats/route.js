a20ac97edada27fa2f946dfc49a5ac7f81e652ef
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-12 18:20:07 +0300

    Add comment for error handling in GET donation stats function

--- src/app/api/donations/stats/route.js
+++ src/app/api/donations/stats/route.js
@@ -3,6 +3,8 @@ import { getDonationStats } from '@/services/donationService';
 
 // Function to GET donation stats
 export async function GET() {
+
+  // Try catch for donation stats error handling
   try {
     const stats = await getDonationStats();
     return NextResponse.json(stats);
