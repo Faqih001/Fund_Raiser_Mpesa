f00c037f7d1f731c9a34c6d2cfe80b0ba58404bb
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-12 18:18:55 +0300

    Add comment for error handling in quick update function

--- src/app/api/donations/quick-update/route.js
+++ src/app/api/donations/quick-update/route.js
@@ -3,6 +3,7 @@ import { updateDonationByCheckoutId } from '@/services/donationService';
 
 // Function to update donation status by checkoutRequestId and mpesaCode
 export async function GET(request) {
+  // Try catch for quick update error handling
   try {
     const { searchParams } = new URL(request.url);
     const checkoutRequestId = searchParams.get('id');
